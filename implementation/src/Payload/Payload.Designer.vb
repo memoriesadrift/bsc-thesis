98448e1be6b8b69eb3a2faf19d81913d7af31d95
Samuel Sulovsky <samuel.sulovsky@gmail.com>
2022-05-23 20:34:03 +0200

    Add Payload Prototype
    This payload is a simple GUI app in vb.net. Might rework it to a different payload as even with trimming this executable is over 80 MB.

---
+++ implementation/src/Payload/Payload.Designer.vb
@@ -0,0 +1,79 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Payload
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 48.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.Location = New System.Drawing.Point(10, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(514, 86)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "You got pwned!"
+        '
+        'Label2
+        '
+        Me.Label2.Enabled = False
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label2.Location = New System.Drawing.Point(126, 157)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(282, 117)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "If this were a real payload, your device would have been infected with a Remote A" &
+    "ccess Trojan (RAT)." & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Luckily, this is just a demonstration of the attack and you" &
+    "r device is safe." & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(223, 296)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(89, 49)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "I Understand"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Payload
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(534, 391)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Payload"
+        Me.Text = "RAT from a BMP"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button1 As Button
+End Class
---
+++ implementation/src/Payload/Payload.vb
@@ -0,0 +1,9 @@
+﻿Public Class Payload
+    Private Sub Settings_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Me.CenterToScreen()
+        Me.BringToFront()
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Close()
+    End Sub
+End Class
