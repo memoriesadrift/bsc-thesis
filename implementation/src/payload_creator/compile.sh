d316d3a923f2dab27adcc840680d5bf9f8785af2
Samuel Šulovský <samuel.sulovsky@gmail.com>
2022-05-25 22:39:01 +0200

    Write short C script to generate JS array for loader
    Takes the .exe and converts it into a data array that the JS loader will use to populate the .exe on the target machine

--- implementation/src/payload_creator/compile.sh
+++ implementation/src/payload_creator/compile.sh
@@ -7,3 +7,11 @@ gcc main.c \
     -O2 \
     -o hide_payload
         
+gcc generate_payload_array.c \
+    -Wall -Wextra -Wpedantic \
+    -Wformat=2 -Wno-unused-parameter -Wshadow \
+    -Wwrite-strings -Wstrict-prototypes -Wold-style-definition \
+    -Wredundant-decls -Wnested-externs -Wmissing-include-dirs \
+    -Wjump-misses-init -Wlogical-op \
+    -O2 \
+    -o generate_payload_array
---
+++ implementation/src/payload_creator/generate_payload_array.c
@@ -0,0 +1,67 @@
+#include <stddef.h>
+#include <stdio.h>  // printf, FILE, fread
+#include <stdlib.h> // NULL, exit, size_t
+#include <string.h> // strlen, strncmp
+
+const char *usage_error =
+    "Usage: ./generate_payload_array payload.exe > dest.js\n";
+
+int check_inputs(char *args[]) {
+  const char *arg1_extension = &args[1][strlen(args[1]) - 4];
+
+  if (strncmp(arg1_extension, ".exe", 4) != 0) {
+    return 0;
+  }
+
+  return 1;
+}
+
+// File Loading with error checking
+FILE *load_file(const char *filename, const char *opts) {
+  FILE *file = fopen(filename, opts);
+
+  if (file == NULL) {
+    printf("Error reading file %s, exiting... \n", filename);
+    exit(1);
+  }
+
+  return file;
+}
+
+int print_array(FILE *file) {
+  size_t read;
+  unsigned short buffer[8192];
+
+  printf("const data = [\n  ");
+  do {
+    read = fread(buffer, 2, sizeof buffer / 2, file);
+    for (size_t i = 0; i < read; i++) {
+      printf("'%d', ", buffer[i]);
+      if (i % 10 == 0 && i != 0) {
+        printf("\n  ");
+      }
+    }
+  } while (read > 0);
+
+  printf("\n]\n");
+  return 1;
+}
+
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    printf("%s", usage_error);
+    return 1;
+  }
+
+  if (!check_inputs(argv)) {
+    printf("%s", usage_error);
+    return 1;
+  }
+
+  FILE *payload = load_file(argv[1], "rb");
+
+  print_array(payload);
+  fclose(payload);
+
+  return 0;
+}
