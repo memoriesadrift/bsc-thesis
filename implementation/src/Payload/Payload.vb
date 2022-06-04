b307619522d51da3421c952848aa1ca5030e62e7
Samuel Šulovský <samuel.sulovsky@gmail.com>
2022-06-04 14:01:48 +0200

    Clean up payload implementation, finalise

--- implementation/src/Payload/Payload.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class Payload
-    Private Sub Settings_Load(sender As Object, e As EventArgs) Handles Me.Load
-        Me.CenterToScreen()
-        Me.BringToFront()
-    End Sub
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Close()
-    End Sub
-End Class
